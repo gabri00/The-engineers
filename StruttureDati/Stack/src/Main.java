755ff23e212b66a077b1e30a28b9adb8013056f0
gabri00 <gibby.nicchia@gmail.com>
2021-10-25 22:22:28 +0200

    Stack nico compilabile

---
+++ StruttureDati/Stack/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main ( String[] args) {
+        Stack s = new Stack(10);
+
+        s.Push(1);
+        s.Push(2);
+        s.Push(3);
+        s.Push(4);
+        System.out.println("La posizione al top é: " +  s.Top());
+        System.out.println("La dimensione della pila é : " + s.Size());
+        System.out.println("Faccio un pop: " + s.Pop());
+    }
+}
---
+++ StruttureDati/Stack/src/Stack.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Stack {
+    private Integer top;
+    private ArrayList <Integer> content;
+
+    public  Stack(Integer n) {
+        this.top = -1;
+        this.content = new ArrayList<Integer>(n);
+    }
+
+    public boolean Is_empty() {
+        return (this.top < 0);
+    }
+
+    public boolean Is_full() {
+        if (this.top == (this.content.size()))
+            return true;
+        else
+            return false;
+    }
+
+    public void Push( Integer i) {
+        if (this.Is_full())
+            return;
+        this.content.add(i);
+        this.top += 1;
+    }
+
+    public Integer Pop() {
+        if (this.Is_empty())
+            return null;
+        Integer n;
+        n = this.content.get(this.top);
+        this.top -= 1 ;
+        return n;
+    }
+
+    public Integer Size () {
+        return this.top + 1;
+    }
+
+    public Integer Top() {
+        if  (this.Is_empty()) {
+            return null;
+        }
+        return this.top;
+    }
+}
